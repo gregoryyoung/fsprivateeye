fdf98babe3880338509c05ffbe58c7a9ec8afa12
Greg Young <gregoryyoung1@gmail.com>
2015-01-28 08:27:04 +0200

    in a broken state but a reasonable time to commit

--- src/crap/host/Program.cs
+++ src/crap/host/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
-
+using System.Text;
 namespace host
 {
     class Program
@@ -12,9 +12,10 @@ namespace host
             
         }
 
-        static void Start()
+        static void BA91E1230BF74A17AB35D3879E65D032()
         {
-            
+           //starts profiling. 
+           Console.WriteLine("*******************************************************************************************************************");
         }
 
         static void Stop()
@@ -24,15 +25,22 @@ namespace host
 
         static void Main(string[] args)
         {
-            using (var stream = new FileStream("/tmp/test", FileMode.Open, FileAccess.Read))
+            Console.WriteLine("starting");
+            Console.WriteLine("calling flush method");
+            BA91E1230BF74A17AB35D3879E65D032();
+            Console.WriteLine("called flush opening file");
+            Console.WriteLine("just some other shit!");
+            using (var stream = new FileInfo("/tmp/test").OpenRead())
             {
+                Console.WriteLine("file open, sending signal to start");
                 var buffer = new byte[4906];
                 while (true)
                 {
                     var read = stream.Read(buffer, 0, buffer.Length);
                     if (read > 0)
                     {
-                        Console.WriteLine("read {0} bytes", read);
+                        var str = Encoding.ASCII.GetString(buffer);
+                        Console.WriteLine("read {0} bytes {1}", read, str);
                     }
                     else
                     {
