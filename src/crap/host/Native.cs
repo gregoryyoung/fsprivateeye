b018aa7586d02c0b561f9ae7ace687ac509491cc
Greg Young <gregoryyoung1@gmail.com>
2015-01-28 06:24:39 -0800

    forgot native

---
+++ src/crap/host/Native.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Win32.SafeHandles;
+
+namespace host
+{
+    public static class Native
+    {
+        public static SafeFileHandle OpenPipeNonblocking(string filename)
+        {
+#if __Mono_CS__
+            Syscalls.open
+#else
+            return new SafeFileHandle(new IntPtr(0), false);
+#endif
+        }
+    }
+}
